7680ef9a259702c897902d2dbb885bfca6e39eb3
SCaddell@my.wctc.edu <>
2013-04-10 21:54:14 -0500

    Lab2 started

---
+++ src/lab2/Contact.java
@@ -0,0 +1,91 @@
+package lab2;
+
+public class Contact {
+    
+    private String fName = null;
+    private String lName = null;
+    private String address = null;
+    private String city = null;
+    private String state = null;
+    private String zipcode = null;
+    private String email = null;
+    private String phoneNum = null;
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+    
+    @Override
+    public String toString() {
+        String str = "fname: " + fName
+                + ", lName: " + lName
+                + ", address: " + address
+                + ", city: " + city
+                + ", state: " + state
+                + ", zipcode: " + zipcode
+                + ", email: " + email
+                + ", phoneNum: " + phoneNum;
+        return str;
+    }
+    
+}
