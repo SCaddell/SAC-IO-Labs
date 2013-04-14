1ee3c8144ad49b001d40f9cf8d26d707973b44a8
SCaddell@my.wctc.edu <>
2013-04-14 18:55:17 -0500

    Reworked Lab1, created Lab1a using a Contact object, Lab1 section 2

--- src/Lab1WithContactObject/Lab1a.java
+++ src/Lab1WithContactObject/Lab1a.java
@@ -54,39 +54,57 @@ public class Lab1a {
         } else {
             System.out.println("File not found: test.txt");
         }
-    }
 
-//        System.out.println("-------------------------------------------");
-//        recCntr = 0;
-//        fieldCntr = 0;
-//
-////        File data = new File(File.separatorChar + "temp" + File.separatorChar
-////                + "class20Lab.txt");
-//        if (data.exists()) {
-//            BufferedReader in = null;
-//            try {
-//                in = new BufferedReader(new FileReader(data));
-//                String line = in.readLine();
-//                while (line != null) {
-//                    fieldCntr++;
-//                    if (fieldCntr >= 9 && fieldCntr <= 16) {
-//                        System.out.println(line);
-//                    }
-//                    line = in.readLine();  // strips out any carriage return chars
-//                }
-//
-//            } catch (IOException ioe) {
-//                System.out.println("Houston, we have a problem! reading this file");
-//            } finally {
-//                try {
-//                    in.close();
-//                } catch (Exception e) {
-//                }
-//            }
-//        } else {
-//            System.out.println("File not found: test.txt");
-//        }
-//        
+        System.out.println("-------------------------------------------");
+        
+        int recCntr = 0;
+        int desiredRec = 2;
+        
+//        File data = new File(File.separatorChar + "temp" + File.separatorChar
+//                + "class20Lab.txt");
+        if (data.exists()) {
+            BufferedReader in = null;
+            try {
+                in = new BufferedReader(new FileReader(data));
+                String line = in.readLine();
+                while (line != null && recCntr < desiredRec) {
+                    recCntr++;
+                    if (recCntr == desiredRec) {
+                        StringTokenizer lineST = new StringTokenizer(line, "|");
+                        String[] contactFields = new String[lineST.countTokens()];
+                        //System.out.println(lineST.countTokens());
+                        int i = 0;
+                        while (lineST.hasMoreTokens()) {
+                            contactFields[i] = lineST.nextToken();
+                            i++;
+                        }
+                        line = in.readLine();  // strips out any carriage return chars
+                        Contact1 contact = new Contact1(contactFields);
+                        //System.out.println(contact.toString());
+                        System.out.println(contact.getfName() + " " 
+                                + contact.getlName());
+                        System.out.println(contact.getAddress());
+                        System.out.println(contact.getCity() + ", " 
+                                + contact.getState() + " "
+                                + contact.getZipcode());
+                        System.out.println(contact.getEmail());
+                        System.out.println(contact.getPhoneNum() + "\n");
+                    }
+                    line = in.readLine();  // strips out any carriage return chars
+                }
+                
+            } catch (IOException ioe) {
+                System.out.println("Houston, we have a problem! reading this file");
+            } finally {
+                try {
+                    in.close();
+                } catch (Exception e) {
+                }
+            }
+        } else {
+            System.out.println("File not found: test.txt");
+        }
+        
 //        System.out.println("-------------------------------------------");
 //        
 //        boolean append = true;   
@@ -179,4 +197,5 @@ public class Lab1a {
 //        }
 //
 //    }
+    }
 }
