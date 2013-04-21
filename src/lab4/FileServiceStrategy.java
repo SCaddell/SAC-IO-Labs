85b0983079b30972592bbe8e207947c1e28ec2f7
SCaddell@my.wctc.edu <>
2013-04-20 23:28:08 -0500

    Lab 4 Saturday1

---
+++ src/lab4/FileServiceStrategy.java
@@ -0,0 +1,18 @@
+
+package lab4;
+
+/**
+ *
+ * @author Stuart Caddell, scaddell@my.wctc.edu
+ */
+public interface FileServiceStrategy {
+    // Marker/tag interface for File Services
+    String getFilePath();
+    
+    void setFilePath(String filePath);
+    
+    FormatStrategy getFormatter();
+    
+    void setFormatter(FormatStrategy formatter);
+    
+}
---
+++ src/lab4/FormatStrategy.java
@@ -0,0 +1,17 @@
+package lab4;
+
+/**
+ * This interface represents the general contract for all Format 
+ * Strategy implementations. The return type and parameters are generic so 
+ * that future implementations and use whatever data type is necessary.
+ * 
+ * @author  Jim Lombardo, jlombardo@wctc.edu
+ * @version 1.00
+ */
+public interface FormatStrategy<T,E> {
+
+    T decode(E dataFromFile);
+
+    String encode(T dataFromFile);
+    
+}
